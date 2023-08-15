8cf3cb2682489e7f93c6b1e115a433f18f4e2161
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-15 22:21:27 +0300

    adding app_theme.dart

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:my_posts_clean_arch/core/utils/app_colors.dart';
+
+
+
+final appTheme = ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primaryColor,
+      centerTitle: true,
+    ),
+    brightness: Brightness.light,
+    primaryColor: AppColors.primaryColor,
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.primaryColor,
+    ),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(color: AppColors.primaryColor),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColors.primaryColor,
+      foregroundColor: AppColors.secondaryColor,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+        floatingLabelStyle: const TextStyle(color: AppColors.primaryColor),
+        iconColor: AppColors.secondaryColor,
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.secondaryColor),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(8),
+        )));
