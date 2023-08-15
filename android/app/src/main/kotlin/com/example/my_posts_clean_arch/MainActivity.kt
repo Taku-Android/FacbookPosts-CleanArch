f4bd0769bd5df991d1fb7870be81d413b1b918b0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-15 15:28:01 +0300

    init the app

---
+++ android/app/src/main/kotlin/com/example/my_posts_clean_arch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_posts_clean_arch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PostApp());
+}
+
+class PostApp extends StatelessWidget {
+  const PostApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
