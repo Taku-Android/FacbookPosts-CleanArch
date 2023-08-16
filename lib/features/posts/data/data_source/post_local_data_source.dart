b2058c02bc8d2524096687e664da6c30c03b50ee
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-17 01:34:08 +0300

    cache the data save it in json and encode it then decode it to list to get posts

--- lib/features/posts/data/data_source/post_local_data_source.dart
+++ lib/features/posts/data/data_source/post_local_data_source.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
+import 'package:my_posts_clean_arch/core/errors/exceptions.dart';
+import 'package:my_posts_clean_arch/core/utils/constants.dart';
 import 'package:my_posts_clean_arch/features/posts/data/model/post_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class PostLocalDataSource{
 
@@ -9,16 +14,32 @@ abstract class PostLocalDataSource{
 }
 
 class PostLocalDataSourceImpl extends PostLocalDataSource{
+
+  final SharedPreferences sharedPreferences ;
+
+  PostLocalDataSourceImpl({required this.sharedPreferences});
+
   @override
   Future<List<PostModel>> getAllCashedPosts() {
-    // TODO: implement getAllCashedPosts
-    throw UnimplementedError();
+      final jsonString = sharedPreferences.getString(kCachedPostsKey);
+      if(jsonString != null){
+        List decodeJsonData = json.decode(jsonString);
+        List<PostModel> jsonToPostModel = decodeJsonData
+            .map<PostModel>((jsonPostModel) => PostModel.fromJson(jsonPostModel))
+            .toList();
+        return Future.value(jsonToPostModel) ;
+      }else{
+        throw EmptyCacheException();
+      }
   }
 
   @override
   Future<Unit> savePostsToCash(List<PostModel> postModels) {
-    // TODO: implement savePostsToCash
-    throw UnimplementedError();
+    List postModelToJson = postModels
+        .map<Map<String , dynamic>>((postModel) => postModel.toJson())
+        .toList();
+    sharedPreferences.setString(kCachedPostsKey, json.encode(postModelToJson));
+    return Future.value(unit) ;
   }
   
 }
