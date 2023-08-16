2ea5db59ffe22941ce29775047a1a96593903fa0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-16 23:16:03 +0300

    remove unnecessary null safty

--- lib/features/posts/domain/entity/post.dart
+++ lib/features/posts/domain/entity/post.dart
@@ -2,9 +2,9 @@ import 'package:equatable/equatable.dart';
 
 class Post extends Equatable {
 
-  final int? id ;
-  final String? title ;
-  final String? body ;
+  final int id ;
+  final String title ;
+  final String body ;
 
   const Post({required this.id , required this.title , required this.body});
 
