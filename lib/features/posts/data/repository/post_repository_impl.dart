0c4228d8352efebac5e5576414fc1122b04eb363
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-16 23:01:08 +0300

    fetch all posts from internet is there is connection if not get the cashed posts

--- lib/features/posts/data/repository/post_repository_impl.dart
+++ lib/features/posts/data/repository/post_repository_impl.dart
@@ -1,22 +1,46 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:my_posts_clean_arch/core/errors/exceptions.dart';
 import 'package:my_posts_clean_arch/core/errors/failure.dart';
 import 'package:my_posts_clean_arch/features/posts/data/data_source/post_local_data_source.dart';
 import 'package:my_posts_clean_arch/features/posts/data/data_source/post_remote_data_source.dart';
 import 'package:my_posts_clean_arch/features/posts/domain/entity/post.dart';
 import 'package:my_posts_clean_arch/features/posts/domain/repository/post_repository.dart';
 
+import '../../../../core/network/internet_checker.dart';
+
 class PostRepositoryImpl extends PostRepo{
 
   final PostRemoteDataSource postRemoteDataSource ;
   final PostLocalDataSource postLocalDataSource ;
+  final InternetChecker internetChecker ;
 
-  PostRepositoryImpl({required this.postRemoteDataSource, required this.postLocalDataSource});
+  PostRepositoryImpl(this.internetChecker, {required this.postRemoteDataSource, required this.postLocalDataSource});
 
 
   @override
-  Future<Either<Failure, List<Post>>> getAllPosts() {
-    // TODO: implement getAllPosts
-    throw UnimplementedError();
+  Future<Either<Failure, List<Post>>> getAllPosts() async {
+    if(await internetChecker.isConnected){
+      try{
+        final remotePosts = await postRemoteDataSource.getAllPosts();
+        postLocalDataSource.savePostsToCash(remotePosts);
+        return right(remotePosts);
+      }catch(e){
+          if(e is DioException){
+            return left(ServerError.fromDioError(e));
+          }else{
+            return left(ServerError(e.toString()));
+
+          }
+      }
+    }else{
+      try{
+        final cashedPosts = await postLocalDataSource.getAllCashedPosts();
+        return right(cashedPosts);
+      }on EmptyCacheException{
+        return left(CashError('No Data'));
+      }
+    }
   }
 
   @override
