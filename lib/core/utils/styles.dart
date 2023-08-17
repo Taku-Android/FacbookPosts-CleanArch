1bb44047ec4987893fc44b4bc2992550321cfa10
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-18 02:35:12 +0300

    adding styles.dart   - for  handling the text

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Styles{
+
+  static  TextStyle textTitle =  const TextStyle(
+    fontSize: 22 ,
+    fontWeight:  FontWeight.bold ,
+    color: Colors.black
+  );
+
+
+  static  TextStyle textDesc =  const TextStyle(
+      fontSize: 18 ,
+      fontWeight:  FontWeight.bold ,
+      color: Colors.grey
+  );
+
+
+  static  TextStyle textId =  const TextStyle(
+      fontSize: 26 ,
+      fontWeight:  FontWeight.bold ,
+      color: Colors.grey
+  );
+
+
+
+}
