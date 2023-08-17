5ccc7cb2a59d770799ac48331547051aed28b29a
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-17 05:01:57 +0300

    init the serviceLocator call my bloc in the material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_posts_clean_arch/core/utils/service_locator.dart';
 import 'package:my_posts_clean_arch/my_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupServiceLocator();
   runApp(const PostApp());
 }
 
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_posts_clean_arch/core/app_theme.dart';
 import 'package:my_posts_clean_arch/core/utils/app_strings.dart';
+import 'package:my_posts_clean_arch/core/utils/service_locator.dart';
+import 'package:my_posts_clean_arch/features/posts/persentation/bloc/get_all_posts_bloc/get_all_posts_cubit.dart';
 
 class PostApp extends StatelessWidget {
   const PostApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: AppStrings.appName,
-      theme: appTheme,
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<GetAllPostsCubit>()),
+
+      ],
+      child: MaterialApp(
+        title: AppStrings.appName,
+        theme: appTheme,
+      ),
     );
   }
 }
