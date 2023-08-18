d6ced1afa2ac65e658bbdbedb6e6cc95ce3175c6
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-18 15:47:51 +0300

    get list item text from the post

--- lib/features/posts/persentation/widgets/post_list_item.dart
+++ lib/features/posts/persentation/widgets/post_list_item.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:my_posts_clean_arch/features/posts/domain/entity/post.dart';
 
 import '../../../../core/utils/styles.dart';
 
 class PostListItem extends StatelessWidget {
-  const PostListItem({super.key});
+  const PostListItem({super.key, required this.post});
+
+  final Post post ;
 
   @override
   Widget build(BuildContext context) {
     return  Column(
       children: [
         ListTile(
-          leading: Text('1' , style: Styles.textId,),
-          title: Text('First title ',style: Styles.textTitle,),
-          subtitle:Text('First description  ',style: Styles.textDesc,) ,
+          leading: Text('${post.id + 1 }' , style: Styles.textId,),
+          title: Text(post.title,style: Styles.textTitle,),
+          subtitle:Text(post.body,style: Styles.textDesc,) ,
 
         ),
         const Divider(height: 2,)
