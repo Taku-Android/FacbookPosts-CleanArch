ca55be3aa09695f6e41dd72d8f1c794d7fbc7ec1
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-18 15:48:10 +0300

    use the bloc to get list of posts from api

--- lib/features/posts/persentation/widgets/post_list_view.dart
+++ lib/features/posts/persentation/widgets/post_list_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_posts_clean_arch/core/utils/loading.dart';
+import 'package:my_posts_clean_arch/features/posts/persentation/bloc/get_all_posts_bloc/get_all_posts_cubit.dart';
+import 'package:my_posts_clean_arch/features/posts/persentation/bloc/get_all_posts_bloc/get_all_posts_state.dart';
 import 'package:my_posts_clean_arch/features/posts/persentation/widgets/post_list_item.dart';
 
 class PostListView extends StatelessWidget {
@@ -6,10 +10,22 @@ class PostListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 6,
-        itemBuilder: (context , index){
-          return const PostListItem();
-        });
+    return BlocBuilder<GetAllPostsCubit, GetAllPostsState>(
+      builder: (context, state) {
+        if(state is GetAllPostsFailure){
+         return ErrorWidget(state.errMessage);
+        }else if(state is GetAllPostsSuccess){
+          var posts = state.posts;
+          return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return  PostListItem(post: posts[index]);
+              });
+        }else{
+          return const LoadingWidget();
+        }
+
+      },
+    );
   }
 }
